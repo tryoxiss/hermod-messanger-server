af7d3e7414a604bbfceab3583868553ff17ba862
tryoxiss <tryoxiss@gmail.com>
2023-06-02 15:10:55 -0700

    Added a doc comment as a packet structure explanation

--- source/packet_handler.rs
+++ source/packet_handler.rs
@@ -49,7 +49,31 @@ struct Packet {
 // }
 
 impl Packet { 
-    fn parse_to_struct(packet_string: &str) -> Packet { 
+
+    /// Packet format: 
+    /// ---
+    /// <OPERATION> <type> "FROM" <target> "WITH" <subject_identifier> ["AND" <encryption algorithum>]
+    /// key: value
+    /// key2: value2
+    /// content: | 
+    /// "This is my content
+    /// uwU
+    /// I love you!!"
+    /// 
+    /// "SIGNED" """<signature>"""
+    /// ---
+    /// Signature always goes at the end. 
+    /// So an example packet is
+    /// GET messages FROM 99f97c79dfae4520a650df014d665be7 WITH bonfire-2023 AND aes
+    /// content: | 
+    /// "This is my content
+    /// uwU
+    /// I love you!!"
+    /// 
+    /// SIGNED "9320ea11f6d427aec4949634dc8676136b2fa8cdad289d22659b44541abb8c51fbeb6b678ded0c9c8a0eec2313192d3a2352b93b4a0e7dbfe29eb5e8dd2e0dcd7f6daf2377a6cbbae6cefdd132536988ad4cea2d36b8334b0a1d928df2341120"
+    /// ---
+    /// Signatre is always at the end. Content can be anywhere (the key value bits can be in any order, but the header (First line) needs to be on the first line always, and the signature always needs to be on the last line)
+    fn raw_to_struct(packet_string: &str) -> Packet { 
         log!("Parsing packet");
 
         return Packet { 
@@ -63,13 +87,13 @@ impl Packet {
         }
     }
 
-    fn new() -> Packet { 
+    fn new(edition: &str, operation: Operation, target: Vec<Guid>, content: &str) -> Packet { 
         return Packet { 
-            edition: String::from("2023"),
-            operation: Operation::Get,
+            edition: String::from(edition),
+            operation: operation,
         
-            target: Vec::new(),
-            content: String::from("packet_string"),
+            target: target,
+            content: String::from(content),
         
             // signature: Signature,
         }
@@ -143,7 +167,7 @@ impl Packet {
 
 // impl Packet for GetRequest { 
 //     fn edition(&self) -> String { 
-//         return self.edition;
+//         return self.edition; 
 //     }
 
 //     fn target(&self) -> Vec<Guid> { 
@@ -159,14 +183,17 @@ pub fn handle_request() {
     log!("Handle Request Called");
     
     let packet;
-    packet = Packet::parse_to_struct("Hiss");
+    packet = Packet::raw_to_struct(
+        "edition: 2023
+        operation: GET
+        ");
 
     println!("{:?}", packet); 
 
 
 
     let packet;
-    packet = Packet::parse_to_struct("I want cuddles");
+    packet = Packet::raw_to_struct("I want cuddles");
 
     println!("{:?}", packet); 
 }
