4e32b1774a06e0446ef508a984dabec96c31846f
tryoxiss <tryoxiss@gmail.com>
2023-06-29 16:19:25 -0700

    ✨ Confirmtation message and reorginisation

--- source/main.rs
+++ source/main.rs
@@ -1,3 +1,25 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  This file is part of:         https://github.com/tryoxiss/bonfire-server *
+ *  Hermod Messanger                          https://en.ourdomain.ext/docs/ *
+ *                                                                           *
+ *  Copyright (C) 2023—present : Hermod Messenger Contributers (AUTHORS.md)  *
+ *                                                                           *
+ *  This program is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU Affero General Public License version 3    *
+ *  as published by the Free Software Foundation.                            *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Affero General Public License version 3 for more details.            *
+ *                                                                           *
+ *  You should have received a copy of the GNU Affero General Public License *
+ *  along with this program.  If not, see:                                   *
+ *    https://www.gnu.org/licenses/agpl-3.0                                  *
+ *                                                                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
 use log::{trace, debug, info, warn, error};
 use log4rs;
 
@@ -18,26 +40,26 @@ mod connection_handler;
 static CODE_START: &str = "\x1b[40m";
 static ENDBLOCK: &str   = "\x1b[0m";
 static INDENT: &str     = "             ";
+static BOLD: &str       = "\x1b[1m";
 
 fn main()
 {
-    info!("Initalising Program");
-
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 
-    trace!("detailed tracing info");
-    debug!("debug info");
-    info!("relevant general info");
-    warn!("warning you unwrap too much");
-    error!("error message here");
+    info!("Initalising Program");
 
-    info!("This is the program speaking now!");
+    // config variables
 
-    // fatal!(901, "I had bad grammer and now I need to fix it");
+    // READ CONFIG FILE
+    // ASSIGN VALUES
+    // NO VALUE? -> DEFULT
 
-    // Define an automatic-restart threashold. 
-    let max_requests: usize = usize::MAX;
-    // 3/4ths to restart
+    let threads: usize = 4;               // threads to add to the pool
+    let max_requests: usize = usize::MAX; // requests before automatic shutdown
+    let listner_ip: &str = "127.0.255.1"; // send requests to this IP
+    let listner_port: &str = "8800";      // Send Requests to this port
+
+    // when do we send a warning the server is reaching its capacity?
     let warn_restart = (max_requests / 4) * 3;
 
     verify_file_integrity();
@@ -47,22 +69,9 @@ fn main()
     // a known security vulnerability (manual override), or running a version
     // nearing the end of support, send a notification/system message
     // telling them. 
-    check_updates();
+    let server_version: String = check_updates();
 
-    info!("Launch Sequence Initated");
-
-    trace!("Initalising TCP Stream");
-
-    let listner_ip = "127.0.255.1";
-    let listner_port = "8800";
-
-    info!("Listening to \x1b[4mhttp://{listner_ip}:{listner_port}{ENDBLOCK}");
-
-    warn!("{CODE_START}network_listner{ENDBLOCK} is bound to an UNWRAPPED VALUE!");
-    let network_listener = TcpListener::bind(format!("{listner_ip}:{listner_port}")).unwrap();
-
-    warn!("TCP Is NOT ENCRYPTED and NOT SPEC COMPLIANT! DIM protocol
-             is actually built in TLS! This is just for testing!");
+    debug!("Launch Sequence Initated");
 
     // ACTUALLY it seems Aes-Gcm-Siv handles all this for us!
     // ---
@@ -80,13 +89,41 @@ fn main()
     // ---
 
     // main portion
-
-    let mut packets_handled: usize = 0;
-    let thread_pool = ThreadPool::new(4);
-
+    debug!("Initalising Thread Pool");
+    let thread_pool = ThreadPool::new(threads);
 
     warn!("When a payload is too lagre (over {} bytes), we simply 
 {INDENT}drop the extra bytes rather than returning a 411 Payload_Too_Large!", u16::MAX);
+
+    debug!("Initalising TCP Stream");
+
+    warn!("TCP Is NOT ENCRYPTED and NOT SPEC COMPLIANT! DIM protocol
+            is actually built in TLS! This is just for testing!");
+    let network_listener = TcpListener::bind(format!("{listner_ip}:{listner_port}"));
+
+    match network_listener
+    {
+        Ok(_) =>
+        {
+            debug!("Network listeer initalised!");
+        }
+
+        Err(error) =>
+        {
+            error!("An error occured when binding to the network listner.
+    {INDENT}Here is the rust compiler message:
+    {error}");
+            panic!();
+        }
+    }
+
+    info!("Your server is running
+{INDENT} -  {BOLD}Software:{ENDBLOCK} {server_version}
+{INDENT} -  {BOLD}Threads:{ENDBLOCK} {threads}
+{INDENT} -  {BOLD}Max Requests:{ENDBLOCK} {max_requests} (warn at 3/4ths though)
+{INDENT} -  {BOLD}Location:{ENDBLOCK} \x1b[4mhttp://{listner_ip}:{listner_port}{ENDBLOCK}
+{INDENT}If this is not correct, please press CTRL+C during the 
+{INDENT}launch countdown to abort the launch.");
 
     // set count to 0 to skip launch sequence
     let launch_countdown: u8 = 0;
@@ -99,7 +136,7 @@ fn main()
         // launching in 9 seccondss
         //                        ^ Stayed because it was never overwritten.
         // We only allow up to a count of 256 (u8), so two trailing spaces is enough.
-        info!("Launching in \x1b[1m{n}{ENDBLOCK} secconds  \x1b[A\r", n=launch_countdown-count);
+        info!("Launching in {BOLD}{n}{ENDBLOCK} secconds  \x1b[A\r", n=launch_countdown-count);
         thread::sleep(Duration::from_secs(1));
     }
 
@@ -110,7 +147,12 @@ fn main()
 {INDENT}It often is, but not always!");
 
     // This automatically persists indefintely.
-    for stream in network_listener.incoming().take(max_requests)
+    // This unwrap is 100% since since we get out of the way earlier if its an Err
+    for (packets_handled, stream) in network_listener
+        .unwrap()
+        .incoming()
+        .enumerate()
+        .take(max_requests)
     {
         match &stream 
         {
@@ -124,8 +166,6 @@ fn main()
                 continue;
             }
         }
-
-        packets_handled += 1;
 
         // This .unwrap() is 100% safe, since we check if its an `Err` type 
         // just above and if it is `continue;` the loop, skipping this block.
@@ -153,9 +193,9 @@ fn main()
 {INDENT}({packets_handled} / {max_requests} packets handled)");
         }
 
+        trace!("lifetime: {packets_handled} packets handled");
 
-        debug!("{packets_handled} packets handled");
-        trace!("lifetime: {packets_handled} / {max_requests}");
+        // println!("\x1b[30mEnter Command ...\x1b[A\r{ENDBLOCK}");
     }
 
     info!("Begining server shutdown ...");
@@ -172,10 +212,10 @@ fn verify_file_integrity()
     let local_software_hash = "Good2Go";
     let server_match_hash = "Good2Go";
 
-    info!("Checking file integrity ...");
+    trace!("Checking file integrity ...");
     if local_software_hash == server_match_hash
     {
-        info!("File integrity good!");
+        debug!("File integrity good!");
     }
     else
     {
@@ -201,7 +241,7 @@ fn verify_file_integrity()
     trace!("Veryfying file integrity");
 }
 
-fn check_updates()
+fn check_updates() -> String
 {
     // get repo from config file
     // get version from config file
@@ -215,6 +255,32 @@ fn check_updates()
     // if version is less than (major): 
     //      suggest update when next doing major admin stuff/setting up new servers
 
+    let project: &str = "hermod";
+    let major: u16 = 0;
+    let minor: u16 = 3;
+    let patch: u16 = 5;
+
+    let release_level: &str = "pre-release";
+    let release_number: u16 = 4;
+
+    let mut release: String = String::from("");
+
+    if release_level != "stable"
+    {
+
+        release = format!(":{release_level}.{release_number}");
+    }
+
+
     warn!("The function {CODE_START}check_updaes(){ENDBLOCK} currently has no functionality.");
     trace!("Checking for Updates");
+
+    return String::from(format!("{project} {major}.{minor}.{patch}{release}",
+            project = project.to_string(),
+            major = major.to_string(),
+            minor = minor.to_string(),
+            patch = patch.to_string(),
+            release = release
+        )
+    );
 }
