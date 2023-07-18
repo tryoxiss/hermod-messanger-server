e0d4ee1d6855695780205cbde293794fe9181baa
tryoxiss <tryoxiss@gmail.com>
2023-07-17 19:10:01 -0700

    🚧 Clean up comments around connection handler

--- source/connection_handler.rs
+++ source/connection_handler.rs
@@ -20,13 +20,13 @@
 
 // use crate::terminal_out;
 
-use log::{trace, debug, info, warn, error};
+use log::{trace, /* debug, info, warn, */ error};
 
 use std::net::TcpStream;
 use std::io::prelude::*;
 
 use aes_gcm_siv::{
-    aead::{Aead, KeyInit, OsRng},
+    aead::{/* Aead, */ KeyInit, OsRng},
     Aes256GcmSiv, Nonce // Or `Aes128GcmSiv`
 };
 
@@ -36,72 +36,49 @@ use crate::{ENDBLOCK, CODE_START, INDENT};
 
 pub fn handle_connection(mut stream: TcpStream)
 {
-    // [0; u16::MAX] (we just cant put a u16 in there because type mismacth.)
-    // A u16::MAX is the maximum packet length. Anything longer must be sent
-    // in multiple packets consecurtive packets. This is specified with a
-    // header flag `packet_series="Name";` and `packet_index=u32`
-    //
-    // This makes the maximum data transfer be:
-    //
-    // Approx: 281,470,681,700,000 Bytes
-    // Approx: or 256 Terrabytes
-    //
-    // be the maximum data transfer in a single packet, but realistically because
-    // of headers and such its only ...
-    //
-    // Approx: 214,748,364,700,000 bytes
-    // Approx: 195.31 Terrabytes
-    //
-    // But if you need to transfer that much data in one sequence I don't think
-    // this is the protocol to do it with, and if it is, then you can just have
-    // something that specifies this continues a previous packet series.
-    //
-    // The packet size limit is to ensure a timely connection for everyone, so
-    // a thread dosen't get stuck on just one request for a long time (making
-    // it easier to DoS or DDoS), it will still get around to other requests
-    // reasonably quickly before returning to processing the bigger request.
-    //
-    // THIS TECHNHECALLY ISN'T DEFINED IN THE SPEC!!! IT DEFINES THAT YOU **MAY**
-    // HAVE A PAYLOAD SIZE LIMIT, AND WAYS TO SPLIT IT INTO MULTIPLE SEPERATE
-    // PAYLOADS, BUT DOES NOT IMPOSE ANY PARTICULAR SIZE LIMITATION!!
-    //
-    // THIS JUST HAPPENS TO BE A CONVIENIENT AND REASONABLE SIZE LIMITATION!!
-    //
-    // This also means machines don't need as much memory per thread.
-    // (Each thread takes about 65.535 or 66 KB of memory at most)
-
-    // TODO: With this current implementation we simply drop extra bytes rather
-    // than returning 411 Payload_Too_Large !!
-
-    // one more than the buffer, so we can see if its null and respond accordingly
-    // 16_777_216
+    /*
+     * [0; u16::MAX] (we just cant put a u16 in there because type mismacth.)
+     * A u16::MAX is the maximum packet length. Anything longer must be sent
+     * in multiple packets consecurtive packets. This is specified with a
+     * header flag `packet_series="Name";` and `packet_index=u32`
+     *
+     * This makes the maximum data transfer be:
+     *
+     * Approx: 281,470,681,700,000 Bytes
+     * Approx: or 256 Terrabytes
+     *
+     * be the maximum data transfer in a single packet, but realistically because
+     * of headers and such its only ...
+     *
+     * Approx: 214,748,364,700,000 bytes
+     * Approx: 195.31 Terrabytes
+     *
+     * But if you need to transfer that much data in one sequence I don't think
+     * this is the protocol to do it with, and if it is, then you can just have
+     * something that specifies this continues a previous packet series.
+     *
+     * The packet size limit is to ensure a timely connection for everyone, so
+     * a thread dosen't get stuck on just one request for a long time (making
+     * it easier to DoS or DDoS), it will still get around to other requests
+     * reasonably quickly before returning to processing the bigger request.
+     *
+     * THIS TECHNHECALLY ISN'T DEFINED IN THE SPEC!!! IT DEFINES THAT YOU **MAY**
+     * HAVE A PAYLOAD SIZE LIMIT, AND WAYS TO SPLIT IT INTO MULTIPLE SEPERATE
+     * PAYLOADS, BUT DOES NOT IMPOSE ANY PARTICULAR SIZE LIMITATION!!
+     *
+     * THIS JUST HAPPENS TO BE A CONVIENIENT AND REASONABLE SIZE LIMITATION!!
+     *
+     * This also means machines don't need as much memory per thread.
+     * (Each thread takes about 65.535 or 66 KB of memory at most)
+     */
 
     const MAX_PACKET_LENGTH: usize = 1048576;
-
     let mut buffer = [0; MAX_PACKET_LENGTH];
 
-    // let mut buffer = vec![];
+    stream.read(&mut buffer)
+        .expect("Failed to read stream!");
 
-    trace!("MEOW");
-
-    match stream.read(&mut buffer)
-    {
-        Ok(message) =>
-        {
-            trace!("Stream Read Successfully");
-        }
-
-        Err(error) =>
-        {
-            error!("The TCP Stream read failed!
-{INDENT}{CODE_START}connection_handler.rs::handle_connection(){ENDBLOCK}
-{INDENT}Here we provide the compilers error:
-{error} ");
-            panic!("Why would the TCP stream flush panic !");
-        }
-    }
-
-    debug!("{:?}", stream);
+    trace!("{:?}", stream);
 
     let key = Aes256GcmSiv::generate_key(&mut OsRng);
     // let cipher = Aes256GcmSiv::new(&key);
@@ -114,11 +91,6 @@ pub fn handle_connection(mut stream: TcpStream)
 
     let cipher = Aes256GcmSiv::new(&key);
     let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
-
-    // warn!("Unsafe {CODE_START}.unwrap(){ENDBLOCK} in {CODE_START}connection_handler.rs{ENDBLOCK}");
-    // let response: Vec<u8> = response.encrypt(nonce, b"plaintext message".as_ref()).unwrap();
-
-    //debug!("buffer: {}; expected: {:?}", buffer[4], "\x00".as_bytes()[0]);
 
     let mut response_variables: Vec<HeaderVariable> = vec![];
 
@@ -141,8 +113,6 @@ pub fn handle_connection(mut stream: TcpStream)
         return;
     }
 
-    // Valid HTTP
-    // let response = format!("HTTP/1.1 200 OK\r\n\r\n Connection established! \n Bufer_Length: {}; \n Packet_Length: {};", buffer.len(), "Unknown");
     let mut header_variables: Vec<HeaderVariable> = vec![];
 
     let header_length: usize = 0;
@@ -161,30 +131,32 @@ pub fn handle_connection(mut stream: TcpStream)
     header_variables.push(HeaderVariable::new("content_formatting", "none"));
     header_variables.push(HeaderVariable::new("time_sent", "2023-06-25 12:25:22"));
 
-    // SUPPORTED TYPES for `content_formatting`
-    // AAA Support (Virtually Required and officailly endorsed)
-    // - none (Plain Text)
-    // - rich-markdown (see DIM Markdown Specification)
-    // - wikitext
-    // - variables (INI Format)
-    //      Chosen because, even if its not your prefered format,
-    //      it's dead simple and does everything we need it to do.
-    //      it dosen't have a bunch of fancy stuff, just 
-    //      key = value ; comment
-    //      NOTE: comments with # are NOT ALLOWED!!
-    //
-    // AA Support (Probably some fancier clients, not offically endorsed)
-    // - commonmark
-    //
-    // A Support (Nieche/Ehh?)
-    // - universal-chess-interface
-    //
-    // E Support (Deprecated)
-    // - None!
-    //
-    // F Support (Actively Discouraged)
-    // - html - DIM Clients are not web browsers!!
-    // - <Any Code> - Use a code block in markdown!!
+    /*
+     * SUPPORTED TYPES for `content_formatting`
+     * AAA Support (Virtually Required and officailly endorsed)
+     * - none (Plain Text)
+     * - rich-markdown (see DIM Markdown Specification)
+     * - wikitext
+     * - variables (INI Format)
+     *      Chosen because, even if its not your prefered format,
+     *      it's dead simple and does everything we need it to do.
+     *      it dosen't have a bunch of fancy stuff, just 
+     *      key = value ; comment
+     *      NOTE: comments with # are NOT ALLOWED!!
+     *
+     * AA Support (Probably some fancier clients, not offically endorsed)
+     * - commonmark
+     *
+     * A Support (Nieche/Ehh?)
+     * - universal-chess-interface
+     *
+     * E Support (Deprecated)
+     * - None!
+     *
+     * F Support (Actively Discouraged)
+     * - html - DIM Clients are not web browsers!!
+     * - <Any Code> - Use a code block in markdown!!
+     */
 
     // DIM
     let response = ResponsePacket::create(
@@ -219,7 +191,7 @@ pub fn handle_connection(mut stream: TcpStream)
 
     match stream.flush()
     {
-        Ok(message) =>
+        Ok(_message) =>
         {
             trace!("TCP Stream Flushed");
         }
@@ -254,6 +226,10 @@ impl HeaderVariable
     }
 }
 
+/// 🚧 Temperary
+/// We will want this for ease of parsing incoming pacekets, but it is
+/// not currently used.
+#[allow(dead_code)]
 struct RequestPacket
 {
     version: String,
@@ -264,9 +240,11 @@ struct RequestPacket
     message: String,
 }
 
-// We need this so that I can impl it
-// It currently does not do anything
-// I may remove this and make it just like CreateResponsePacket or something
+/// 📔 Note
+/// The reason none of these variables is read is because this object exists
+/// for easier packet construction. We make the packet, and then write to
+/// the stream `.as_bytes()`!
+#[allow(dead_code)]
 struct ResponsePacket
 {
     version: String,
@@ -315,8 +293,6 @@ impl ResponsePacket
 #[cfg(test)]
 mod tests
 {
-    use log::trace;
-
     use crate::connection_handler::ResponsePacket;
     use crate::connection_handler::HeaderVariable;
 
