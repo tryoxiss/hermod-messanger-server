f64c685a14b6e57466abfcb3a63d7668d15f6a40
tryoxiss <tryoxiss@gmail.com>
2023-09-14 16:42:46 -0700

    🚧 Spaces -> Tabs

--- source/_connection_handler.rs
+++ source/_connection_handler.rs
@@ -1,8 +1,8 @@
 /*
- *      This file is part of:
- *      Codename Bonfire Instant Messanger
+ *	  This file is part of:
+ *	  Codename Bonfire Instant Messanger
  *
- *      Copyright (C) 2023 or later - Project Bonfire Contributers
+ *	  Copyright (C) 2023 or later - Project Bonfire Contributers
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -29,162 +29,162 @@ use crate::{ENDBLOCK, CODE_START, INDENT};
 
 pub fn handle_connection(mut stream: TlsStream<TcpStream>)
 {
-    // the request can be returned in such a way that an error was found in process_incoming()
-    let (request, stream) = process_incoming(stream);
+	// the request can be returned in such a way that an error was found in process_incoming()
+	let (request, stream) = process_incoming(stream);
 
-    // if there was a problem in process_incoming(), construct the error.
-    let response = handle_request(request);
-    return_result(response, stream)
+	// if there was a problem in process_incoming(), construct the error.
+	let response = handle_request(request);
+	return_result(response, stream)
 }
 
 fn create_header_variables() -> Vec<HeaderVariable>
 {
-    let mut header_variables: Vec<HeaderVariable> = vec![];
+	let mut header_variables: Vec<HeaderVariable> = vec![];
 
-    let header_length: usize = 0;
-    for variable in 0..header_length
-    {
-        header_variables.push(HeaderVariable::new("key", "value"));
-    }
+	let header_length: usize = 0;
+	for variable in 0..header_length
+	{
+		header_variables.push(HeaderVariable::new("key", "value"));
+	}
 
 
-    // Use Aes-Gcm-Siv for Client-to-Server (aka not Peer-to-Peer)
-    // ---
-    // We can use a CSPRNG or Crypographically Secure Psudo-Random Number
-    // Generator for encryption values. We will use ChaCha20-poly1305 since
-    // - It can produce 1.8gb of randomness every seccond, making it far from
-    //   a bottleneck.
-    // - initalises fast (but startup times are not very important)
-    // - only uses 136 bytes of perpetual memory 
-    // - has been [deeply analyised](ChaCha20Analysis) 
-    // 
-    // [ChaCha20Analysis]: https://datatracker.ietf.org/doc/html/rfc7539#section-1
-    // (Same as Above)   : https://www.cryptrec.go.jp/exreport/cryptrec-ex-2601-2016.pdf
-    // (Summary)         : https://en.wikipedia.org/wiki/ChaCha20-Poly1305
-    // ---
+	// Use Aes-Gcm-Siv for Client-to-Server (aka not Peer-to-Peer)
+	// ---
+	// We can use a CSPRNG or Crypographically Secure Psudo-Random Number
+	// Generator for encryption values. We will use ChaCha20-poly1305 since
+	// - It can produce 1.8gb of randomness every seccond, making it far from
+	//   a bottleneck.
+	// - initalises fast (but startup times are not very important)
+	// - only uses 136 bytes of perpetual memory 
+	// - has been [deeply analyised](ChaCha20Analysis) 
+	// 
+	// [ChaCha20Analysis]: https://datatracker.ietf.org/doc/html/rfc7539#section-1
+	// (Same as Above)   : https://www.cryptrec.go.jp/exreport/cryptrec-ex-2601-2016.pdf
+	// (Summary)		 : https://en.wikipedia.org/wiki/ChaCha20-Poly1305
+	// ---
 
-    header_variables.push(HeaderVariable::new("encyption", "aes"));
-    header_variables.push(HeaderVariable::new("force_encryption", "t"));
-    header_variables.push(HeaderVariable::new("author", "8d1a0cfb13df4ca3bdb0e912be01863b"));
-    header_variables.push(HeaderVariable::new("target", "none"));
-    header_variables.push(HeaderVariable::new("channel", "20026f0a1c484f95a0063d148c8898f9"));
-    header_variables.push(HeaderVariable::new("channel_type", "text_message"));
-    header_variables.push(HeaderVariable::new("content_mime_type", "text/plain"));
-    header_variables.push(HeaderVariable::new("content_formatting", "none"));
-    header_variables.push(HeaderVariable::new("time_sent", "2023-06-25 12:25:22"));
+	header_variables.push(HeaderVariable::new("encyption", "aes"));
+	header_variables.push(HeaderVariable::new("force_encryption", "t"));
+	header_variables.push(HeaderVariable::new("author", "8d1a0cfb13df4ca3bdb0e912be01863b"));
+	header_variables.push(HeaderVariable::new("target", "none"));
+	header_variables.push(HeaderVariable::new("channel", "20026f0a1c484f95a0063d148c8898f9"));
+	header_variables.push(HeaderVariable::new("channel_type", "text_message"));
+	header_variables.push(HeaderVariable::new("content_mime_type", "text/plain"));
+	header_variables.push(HeaderVariable::new("content_formatting", "none"));
+	header_variables.push(HeaderVariable::new("time_sent", "2023-06-25 12:25:22"));
 
-    return header_variables;
+	return header_variables;
 }
 
 fn process_incoming(mut stream: TlsStream<TcpStream>) -> (RequestPacket, TlsStream<TcpStream>)
 {
-    return (RequestPacket::new(), stream);
+	return (RequestPacket::new(), stream);
 }
 
 fn handle_request(request: RequestPacket) -> ResponsePacket
 {
-    let compliance_vars = create_header_variables();
+	let compliance_vars = create_header_variables();
 
-    return ResponsePacket
-    {
-        version: "1.0".to_string(),
-        response_code: 501,
-        header_flags: compliance_vars,
-        response_message: "Not Implemented".to_string(),
-        body: "Sorry :/".to_string()
-    }
+	return ResponsePacket
+	{
+		version: "1.0".to_string(),
+		response_code: 501,
+		header_flags: compliance_vars,
+		response_message: "Not Implemented".to_string(),
+		body: "Sorry :/".to_string()
+	}
 }
 
 fn return_result(packet: ResponsePacket, mut stream: TlsStream<TcpStream>) -> ()
 {
-    let response_variables = create_header_variables();
+	let response_variables = create_header_variables();
 
-    let response = ResponsePacket::create(
-        "1.0",
-        200,
-        "Serving",
-        response_variables,
-        "Manically laughs at the futility of life. Oh also I got DIM packets sorta being contructed!"
-    );
+	let response = ResponsePacket::create(
+		"1.0",
+		200,
+		"Serving",
+		response_variables,
+		"Manically laughs at the futility of life. Oh also I got DIM packets sorta being contructed!"
+	);
 
-    // Writes some prefix of the byte string, not necessarily all of it.
-    stream.write(response.as_bytes()).unwrap();
+	// Writes some prefix of the byte string, not necessarily all of it.
+	stream.write(response.as_bytes()).unwrap();
 
-    match stream.flush()
-    {
-        Ok(_message) =>
-        {
-            trace!("TCP Stream Flushed");
-        }
+	match stream.flush()
+	{
+		Ok(_message) =>
+		{
+			trace!("TCP Stream Flushed");
+		}
 
-        Err(error)  =>
-        {
-            error!("The TCP Stream flush failed!
+		Err(error)  =>
+		{
+			error!("The TCP Stream flush failed!
 {INDENT}{CODE_START}connection_handler.rs::handle_connection(){ENDBLOCK}
 {INDENT}Here we provide the compilers error:
 {error} ");
-        }
-    }
+		}
+	}
 
-    return ()
+	return ()
 }
 
 #[derive(Debug)]
 struct HeaderVariable
 {
-    key: String,
-    value: String,
+	key: String,
+	value: String,
 }
 
 impl HeaderVariable
 {
-    fn new(key: &str, value: &str) -> HeaderVariable
-    {
-        HeaderVariable
-        {
-            key: key.to_string(),
-            value: value.to_string(),
-        }
-    }
+	fn new(key: &str, value: &str) -> HeaderVariable
+	{
+		HeaderVariable
+		{
+			key: key.to_string(),
+			value: value.to_string(),
+		}
+	}
 }
 
 enum RequestMethod
 {
-    Get,
-    Edit,
-    Post,
-    Remove,
+	Get,
+	Edit,
+	Post,
+	Remove,
 
-    // not an actual method, simply used to indicate that the content
-    // is to be taken as a parsable error and not literal content.
-    InternalServerError
+	// not an actual method, simply used to indicate that the content
+	// is to be taken as a parsable error and not literal content.
+	InternalServerError
 }
 
 struct RequestPacket
 {
-    version: String,
-    method: RequestMethod,
-    header_flags: Vec<HeaderVariable>,
-    resource_location: String,     // MAYBE REQUEST DEPTH? So you can like get a group without getting all channels and messages in it.
+	version: String,
+	method: RequestMethod,
+	header_flags: Vec<HeaderVariable>,
+	resource_location: String,	 // MAYBE REQUEST DEPTH? So you can like get a group without getting all channels and messages in it.
 
-    body: String,
+	body: String,
 }
 
 impl RequestPacket
 {
-    fn new() -> RequestPacket
-    {
-        let compliance_vars = create_header_variables();
+	fn new() -> RequestPacket
+	{
+		let compliance_vars = create_header_variables();
 
-        return RequestPacket
-        {
-            version: "1.0".to_string(),
-            method: RequestMethod::Get,
-            header_flags: compliance_vars,
-            resource_location: "hiss".to_string(),
-            body: "meow".to_string()
-        }
-    }
+		return RequestPacket
+		{
+			version: "1.0".to_string(),
+			method: RequestMethod::Get,
+			header_flags: compliance_vars,
+			resource_location: "hiss".to_string(),
+			body: "meow".to_string()
+		}
+	}
 }
 
 /// 📔 Note
@@ -194,74 +194,74 @@ impl RequestPacket
 #[allow(dead_code)]
 struct ResponsePacket
 {
-    version: String,
-    response_code: u16,
-    header_flags: Vec<HeaderVariable>,
-    response_message: String,
+	version: String,
+	response_code: u16,
+	header_flags: Vec<HeaderVariable>,
+	response_message: String,
 
-    body: String, // Anything that implements Resource
+	body: String, // Anything that implements Resource
 }
 
 impl ResponsePacket
 {
-    fn create(version: &str,
-        response_code: u16,
-        response_message: &str,
-        header_variables: Vec<HeaderVariable>,
-        content: &str) -> String
-    {
-        let response_header = format!("dim/{version} {code} {response_message}\n",
-            version=version.to_string(),
-            code=response_code.to_string(),
-            response_message=response_message.to_string()
-        );
-        let mut response_variables: String = String::from("");
+	fn create(version: &str,
+		response_code: u16,
+		response_message: &str,
+		header_variables: Vec<HeaderVariable>,
+		content: &str) -> String
+	{
+		let response_header = format!("dim/{version} {code} {response_message}\n",
+			version=version.to_string(),
+			code=response_code.to_string(),
+			response_message=response_message.to_string()
+		);
+		let mut response_variables: String = String::from("");
 
-        for variable in header_variables.iter()
-        {
-            trace!("variable: {:?}", variable);
+		for variable in header_variables.iter()
+		{
+			trace!("variable: {:?}", variable);
 
-            // I can't += this for some reason ...
-            response_variables = format!("{pre}{key}={value};", pre=response_variables, key=variable.key, value=variable.value);
-        }
+			// I can't += this for some reason ...
+			response_variables = format!("{pre}{key}={value};", pre=response_variables, key=variable.key, value=variable.value);
+		}
 
-        trace!("{:?}", response_variables);
+		trace!("{:?}", response_variables);
 
-        let packet = format!("{response_header}{response_variables}\n{content}");
+		let packet = format!("{response_header}{response_variables}\n{content}");
 
-        trace!("{:?}", packet);
+		trace!("{:?}", packet);
 
-        return packet.to_string();
+		return packet.to_string();
 
-        // return response_header.as_bytes() + response_variables.as_bytes() + response_message.as_bytes();
-    }
+		// return response_header.as_bytes() + response_variables.as_bytes() + response_message.as_bytes();
+	}
 
-    fn error_response(mut stream: TlsStream<TcpStream>, version: &str, response_code: u16, response_message: &str, content: &str)
-    {
-        let mut header_variables: Vec<HeaderVariable> = vec![];
+	fn error_response(mut stream: TlsStream<TcpStream>, version: &str, response_code: u16, response_message: &str, content: &str)
+	{
+		let mut header_variables: Vec<HeaderVariable> = vec![];
 
-        header_variables.push(HeaderVariable::new("encyption", "aes"));
-        header_variables.push(HeaderVariable::new("force_encryption", "t"));
-        // header_variables.push(HeaderVariable::new("author", "8d1a0cfb13df4ca3bdb0e912be01863b"));
-        // header_variables.push(HeaderVariable::new("target", "none"));
-        // header_variables.push(HeaderVariable::new("channel", "20026f0a1c484f95a0063d148c8898f9"));
-        // header_variables.push(HeaderVariable::new("channel_type", "text_message"));
-        header_variables.push(HeaderVariable::new("content_mime_type", "text/plain"));
-        header_variables.push(HeaderVariable::new("content_formatting", "none"));
-        header_variables.push(HeaderVariable::new("time_sent", "2023-06-25 12:25:22"));
-        // -- send an error response. Do not return.
-        // use a set of pre-existing 
+		header_variables.push(HeaderVariable::new("encyption", "aes"));
+		header_variables.push(HeaderVariable::new("force_encryption", "t"));
+		// header_variables.push(HeaderVariable::new("author", "8d1a0cfb13df4ca3bdb0e912be01863b"));
+		// header_variables.push(HeaderVariable::new("target", "none"));
+		// header_variables.push(HeaderVariable::new("channel", "20026f0a1c484f95a0063d148c8898f9"));
+		// header_variables.push(HeaderVariable::new("channel_type", "text_message"));
+		header_variables.push(HeaderVariable::new("content_mime_type", "text/plain"));
+		header_variables.push(HeaderVariable::new("content_formatting", "none"));
+		header_variables.push(HeaderVariable::new("time_sent", "2023-06-25 12:25:22"));
+		// -- send an error response. Do not return.
+		// use a set of pre-existing 
 
-        let response = ResponsePacket::create(
-            version,
-            response_code,
-            response_message,
-            header_variables,
-            content
-        );
+		let response = ResponsePacket::create(
+			version,
+			response_code,
+			response_message,
+			header_variables,
+			content
+		);
 
-        stream.write(response.as_bytes()).unwrap();
-    }
+		stream.write(response.as_bytes()).unwrap();
+	}
 }
 
 //
@@ -271,17 +271,17 @@ impl ResponsePacket
 #[cfg(test)]
 mod tests
 {
-    use crate::_connection_handler::ResponsePacket;
-    use crate::_connection_handler::HeaderVariable;
+	use crate::_connection_handler::ResponsePacket;
+	use crate::_connection_handler::HeaderVariable;
 
-    #[test]
-    fn no_header_variables()
-    {
-        let response_variables: Vec<HeaderVariable> = vec![];
+	#[test]
+	fn no_header_variables()
+	{
+		let response_variables: Vec<HeaderVariable> = vec![];
 
-        assert_eq!(
-            "dim/1.0 200 Serving\nTest content",
-            ResponsePacket::create("1.0", 200, "Serving", response_variables, "Test content")
-        )
-    }
+		assert_eq!(
+			"dim/1.0 200 Serving\nTest content",
+			ResponsePacket::create("1.0", 200, "Serving", response_variables, "Test content")
+		)
+	}
 }
--- source/_packet_handler.rs
+++ source/_packet_handler.rs
@@ -1,22 +1,22 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *  This file is part of:         https://github.com/tryoxiss/bonfire-server *
- *  Hermod Messanger                          https://en.ourdomain.ext/docs/ *
- *                                                                           *
+ *  This file is part of:		 https://github.com/tryoxiss/bonfire-server *
+ *  Hermod Messanger						  https://en.ourdomain.ext/docs/ *
+ *																		   *
  *  Copyright (C) 2023—present : Hermod Messenger Contributers (AUTHORS.md)  *
- *                                                                           *
- *  This program is free software: you can redistribute it and/or modify     *
- *  it under the terms of the GNU Affero General Public License version 3    *
- *  as published by the Free Software Foundation.                            *
- *                                                                           *
- *  This program is distributed in the hope that it will be useful,          *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
- *  GNU Affero General Public License version 3 for more details.            *
- *                                                                           *
+ *																		   *
+ *  This program is free software: you can redistribute it and/or modify	 *
+ *  it under the terms of the GNU Affero General Public License version 3	*
+ *  as published by the Free Software Foundation.							*
+ *																		   *
+ *  This program is distributed in the hope that it will be useful,		  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of		   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			*
+ *  GNU Affero General Public License version 3 for more details.			*
+ *																		   *
  *  You should have received a copy of the GNU Affero General Public License *
- *  along with this program.  If not, see:                                   *
- *    https://www.gnu.org/licenses/agpl-3.0                                  *
- *                                                                           *
+ *  along with this program.  If not, see:								   *
+ *	https://www.gnu.org/licenses/agpl-3.0								  *
+ *																		   *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* @authors
  *  *  tryoxiss 'madeline'
@@ -30,28 +30,28 @@
 #[derive(Debug)]
 struct HeaderVariable
 {
-    key: String,
-    value: String,
+	key: String,
+	value: String,
 }
 
 impl HeaderVariable
 {
-    fn new(key: &str, value: &str) -> HeaderVariable
-    {
-        HeaderVariable
-        {
-            key: key.to_string(),
-            value: value.to_string(),
-        }
-    }
+	fn new(key: &str, value: &str) -> HeaderVariable
+	{
+		HeaderVariable
+		{
+			key: key.to_string(),
+			value: value.to_string(),
+		}
+	}
 }
 
 enum RequestType
 {
-    Get,
-    Edit,
-    Post,
-    Remove
+	Get,
+	Edit,
+	Post,
+	Remove
 }
 
 /// 🚧 Temperary
@@ -60,57 +60,57 @@ enum RequestType
 #[allow(dead_code)]
 struct RequestPacket
 {
-    version: String,
-    request_type: RequestType,
-    requested_resource: String,
-    header_flags: Vec<HeaderVariable>,
+	version: String,
+	request_type: RequestType,
+	requested_resource: String,
+	header_flags: Vec<HeaderVariable>,
 
-    message: String,
+	message: String,
 }
 
 impl RequestPacket
 {
-    fn parse( /* mut stream: TlsStream<TcpStream>, */ packet: &str)
-    {
-        let mut version: &str = "";
-        let mut request_type: &str = "";
-        let mut requested_resource: &str = "";
-        let mut header_flags: &str = "";
-        let mut message: &str = "";
+	fn parse( /* mut stream: TlsStream<TcpStream>, */ packet: &str)
+	{
+		let mut version: &str = "";
+		let mut request_type: &str = "";
+		let mut requested_resource: &str = "";
+		let mut header_flags: &str = "";
+		let mut message: &str = "";
 
-        // Deserialize packet into structure for future use
+		// Deserialize packet into structure for future use
 
-        packet.to_string();
-        let packet = packet.split_at(packet.clone().find(" ").unwrap());
-        version = packet.0;
+		packet.to_string();
+		let packet = packet.split_at(packet.clone().find(" ").unwrap());
+		version = packet.0;
 
-        let packet = packet.1.split_at(packet.1.clone().find(" ").unwrap());
-        request_type = packet.0;
+		let packet = packet.1.split_at(packet.1.clone().find(" ").unwrap());
+		request_type = packet.0;
 
-        // TODO: Needs to handle GUIDs and usernames seperately as well as telling if group or user
-        let packet = packet.1.split_at(packet.1.clone().find(" ").unwrap());
-        requested_resource = packet.0;
+		// TODO: Needs to handle GUIDs and usernames seperately as well as telling if group or user
+		let packet = packet.1.split_at(packet.1.clone().find(" ").unwrap());
+		requested_resource = packet.0;
 
-        // Header flags
+		// Header flags
 
-        // Message
+		// Message
 
-        match request_type
-        {
-            // its ugly but we want to shadow request_type to save memory
-            "GET"    => { let request_type: RequestType = RequestType::Get; },
-            "POST"   => { let request_type: RequestType = RequestType::Post; },
-            "EDIT"   => { let request_type: RequestType = RequestType::Edit; },
-            "REMOVE" => { let request_type: RequestType = RequestType::Remove; },
-            // _        => ResponsePacket::error_response(
-            //     stream, "1.0",
-            //     401,
-            //     "Invalid Method",
-            //     ""
-            // ),
-            _ => {println!("OOPSY DOOPSY ! deserialization.rs line ~100")}
-        }
-    }
+		match request_type
+		{
+			// its ugly but we want to shadow request_type to save memory
+			"GET"	=> { let request_type: RequestType = RequestType::Get; },
+			"POST"   => { let request_type: RequestType = RequestType::Post; },
+			"EDIT"   => { let request_type: RequestType = RequestType::Edit; },
+			"REMOVE" => { let request_type: RequestType = RequestType::Remove; },
+			// _		=> ResponsePacket::error_response(
+			//	 stream, "1.0",
+			//	 401,
+			//	 "Invalid Method",
+			//	 ""
+			// ),
+			_ => {println!("OOPSY DOOPSY ! deserialization.rs line ~100")}
+		}
+	}
 }
 
 //
@@ -124,34 +124,34 @@ mod tests
 // user: @username, guid
 // group: guid/channel, guid/guid
 
-    use crate::packet_handler::RequestPacket;
+	use crate::packet_handler::RequestPacket;
 
-    #[test]
-    fn deserialization()
-    {
-        let input_packet = "dim/1.0 GET user:ffb6735ac32f4f6caefb265352d87f6f
+	#[test]
+	fn deserialization()
+	{
+		let input_packet = "dim/1.0 GET user:ffb6735ac32f4f6caefb265352d87f6f
 encyption=aes;force_encryption=t;author=8d1a0cfb13df4ca3bdb0e912be01863b;target=none;channel=20026f0a1c484f95a0063d148c8898f9;channel_type=text_message;content_mime_type=text/plain;content_formatting=none;time_sent=2023-06-25 12:25:22;
 Manically laughs at the futility of life. Oh also I got DIM packets sorta being contructed!";
 
-        let request_packet = RequestPacket::parse(input_packet);
+		let request_packet = RequestPacket::parse(input_packet);
 
-        println!("{:?}", request_packet);
-    }
+		println!("{:?}", request_packet);
+	}
 }
 
 // struct Resource
 // {
-//     name: String,
-//     data: Vec<u8>, // byte vector
+//	 name: String,
+//	 data: Vec<u8>, // byte vector
 // }
 
 enum ResourceLocation
 {
-    ResourceHierLocation(String),
-    ResourceGuidLocation(u128), // GUID
+	ResourceHierLocation(String),
+	ResourceGuidLocation(u128), // GUID
 }
 
 trait Resource
 {
-    fn load() {}
+	fn load() {}
 }
